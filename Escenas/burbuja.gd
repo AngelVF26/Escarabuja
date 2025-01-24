8d1fe3753adf1ce91c183995d49330bdfd78ce1d
Azzazel <138659938+AngelVF26@users.noreply.github.com>
2025-01-24 22:51:03 +0100

    primas colisiones areas

--- Escenas/burbuja.gd
+++ Escenas/burbuja.gd
@@ -1,11 +1,12 @@
 extends Area2D
 
-@onready var bichote: CharacterBody2D = $"."
-@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var bichote: CharacterBody2D = $bichote
 
+var bichejo_empuja: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +14,11 @@ func _process(delta: float) -> void:
 	
 	
 	pass
+
+func _physics_process(delta: float) -> void:
+	pass
+
+func _on_body_exited(body: Node2D) -> void:
+
+		
+	pass # Replace with function body.
