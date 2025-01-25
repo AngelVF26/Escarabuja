4438344a86534f1b1149202e44cb41da24d15082
Azzazel <138659938+AngelVF26@users.noreply.github.com>
2025-01-25 16:18:29 +0100

    pos na un sacabao pass

--- Escenas/bichote.gd
+++ Escenas/bichote.gd
@@ -32,7 +32,6 @@ func _physics_process(delta: float) -> void:
 		velocity.y = -SPEED * 0.8
 	
 	move_and_slide()
-
-
-func _on_timer_timeout() -> void:
-	pass # Replace with function body.
+	
+func sacabao():
+	pass
