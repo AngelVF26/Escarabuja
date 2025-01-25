30aca05a0b33972ff15c916abe8e9258ee590e2c
Javikh33 <72210148+Javikh33@users.noreply.github.com>
2025-01-25 22:01:28 +0100

    matamoscas

--- Escenas/matamoscas.gd
+++ Escenas/matamoscas.gd
@@ -4,6 +4,8 @@ var nombre: String = "Matamoscas"
 # Called when the node enters the scene tree for the first time.
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@onready var animated_moscas: AnimatedSprite2D = $AnimatedMoscas
+@onready var area_2d: Area2D = $Area2D
 
 
 func _physics_process(delta: float) -> void:
@@ -20,3 +22,10 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_released("pabajo"):
 		velocity.y = SPEED * 0.8
 	move_and_slide()
+	
+	if animated_moscas.frame != 4:
+		$Area2D/CollisionShape2D.set_deferred("disabled", true)
+		$CollisionShape2D.set_deferred("disabled", true)
+	else:
+		$Area2D/CollisionShape2D.set_deferred("disabled", false)
+		$CollisionShape2D.set_deferred("disabled", false)
