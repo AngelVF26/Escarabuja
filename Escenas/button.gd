6b46866a30ad2be995bebd4b3ec53596ecdf50e6
Azzazel <138659938+AngelVF26@users.noreply.github.com>
2025-01-25 13:20:26 +0100

    tonterias inicio

--- Escenas/button.gd
+++ Escenas/button.gd
@@ -12,4 +12,4 @@ func _process(delta: float) -> void:
 
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://Escenas/burbujea.tscn")
+	get_tree().change_scene_to_file("res://Escenas/pantalla.tscn")
