a68ec4bd291153f67fee3db9ae8f0d5fe58f94ae
Azzazel <72210148+Javikh33@users.noreply.github.com>
2025-01-25 14:25:22 +0100

    s

--- Escenas/burbuja_pickup.gd
+++ Escenas/burbuja_pickup.gd
@@ -12,4 +12,9 @@ func _physics_process(delta: float) -> void:
 	#var direction_y:=Input.get_axis("parriba", "pabajo")
 	velocity.y = SPEED * 0.8
 	
+	if Input.is_action_pressed("pabajo"):
+		if !Input.is_action_pressed("sprint"):
+			velocity.y = SPEED * 0.3
+	if Input.is_action_just_released("pabajo"):
+		velocity.y = SPEED * 0.8
 	move_and_slide()
