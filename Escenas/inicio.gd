1b7f051c49b3b19774edfc7ad482fb235cc495db
Azzazel <138659938+AngelVF26@users.noreply.github.com>
2025-01-26 07:11:13 +0100

    Instrucciones

--- Escenas/inicio.gd
+++ Escenas/inicio.gd
@@ -62,3 +62,10 @@ func _on_button_pressed() -> void:
 	$AudioStreamPlayer.play()
 	get_tree().change_scene_to_file("res://Escenas/pantalla.tscn")
 	pass # Replace with function body.
+
+
+func _on_boton_instrucciones_pressed() -> void:
+	$AudioStreamPlayer.play()
+	get_tree().change_scene_to_file("res://Escenas/instrucciones.tscn")
+	
+	pass # Replace with function body.
