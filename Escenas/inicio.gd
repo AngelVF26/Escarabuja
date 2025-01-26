bde1f7a11ea9e5df7b4da730e126fe83212ef091
Azzazel <138659938+AngelVF26@users.noreply.github.com>
2025-01-26 03:57:40 +0100

    Super GRAN animacion Inicio

--- Escenas/inicio.gd
+++ Escenas/inicio.gd
@@ -57,3 +57,7 @@ func read(file: FileAccess):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Escenas/pantalla.tscn")
+	pass # Replace with function body.
