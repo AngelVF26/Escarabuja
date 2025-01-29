265b4c5fa1421f8827c87bf340fd85174d83f18c
Javikh33 <72210148+Javikh33@users.noreply.github.com>
2025-01-29 18:35:15 +0100

    explosiones

--- Escenas/bichote.gd
+++ Escenas/bichote.gd
@@ -8,12 +8,11 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction_x := Input.get_axis("paizquierda", "paderecha")
 	var direction_y:=Input.get_axis("parriba", "pabajo")
-	velocity.y = -SPEED * 0.8
+	#velocity.y = -SPEED * 0.8
 	velocity.x = direction_x * SPEED
 	if Input.is_action_pressed("paizquierda"):
 		rotation_degrees = -15
@@ -23,15 +22,17 @@ func _physics_process(delta: float) -> void:
 		rotation_degrees = 0
 	if Input.is_action_just_released("paizquierda"):
 		rotation_degrees = 0
-	if Input.is_action_pressed("sprint"):
-		velocity.y = -SPEED *1.5
+	if Input.is_action_pressed("sprint") && direction_x == -1:
+		velocity.x = -SPEED *2
+	if Input.is_action_pressed("sprint") && direction_x == 1:
+		velocity.x = SPEED *2
 	if Input.is_action_just_released("sprint"):
-		velocity.y = direction_y * SPEED
-	if Input.is_action_pressed("pabajo"):
-		if !Input.is_action_pressed("sprint"):
-			velocity.y = -SPEED * 0.3
-	if Input.is_action_just_released("pabajo"):
-		velocity.y = -SPEED * 0.8
+		velocity.x = direction_x * SPEED
+	#if Input.is_action_pressed("pabajo"):
+		#if !Input.is_action_pressed("sprint"):
+			#velocity.y = -SPEED * 0.3
+	#if Input.is_action_just_released("pabajo"):
+		#velocity.y = -SPEED * 0.8
 	
 	move_and_slide()
 	
